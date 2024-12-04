7223f5f9546f703f52b277cd940cba65e6b6771b
Hakeem Abbas <hakeemsyd@gmail.com>
2024-12-04 12:50:58 +0500

    Migrations: removed the unneccesary index on phone_number.

--- src/database/migrations/1731490038579-user-entity-remove-unique-phone.ts
+++ src/database/migrations/1731490038579-user-entity-remove-unique-phone.ts
@@ -10,22 +10,12 @@ export class UserEntityRemoveUniquePhone1731490038579
     await queryRunner.query(
       `ALTER TABLE "user" DROP CONSTRAINT "UQ_01eea41349b6c9275aec646eee0"`,
     );
-
-    // Drop the index associated with phone_number column
-    await queryRunner.query(
-      `DROP INDEX "public"."IDX_01eea41349b6c9275aec646eee"`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     // Re-add the unique constraint on phone_number column
     await queryRunner.query(
       `ALTER TABLE "user" ADD CONSTRAINT "UQ_01eea41349b6c9275aec646eee0" UNIQUE ("phone_number")`,
-    );
-
-    // Re-create the index on phone_number column
-    await queryRunner.query(
-      `CREATE INDEX "IDX_01eea41349b6c9275aec646eee" ON "user" ("phone_number")`,
     );
   }
 }
