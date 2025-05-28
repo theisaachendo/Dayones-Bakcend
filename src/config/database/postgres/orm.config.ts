866b28524bbacb85eadc64b5482ceb19a23e3d05
Isaac Henderson <theisaachendo@gmail.com>
2025-05-28 18:29:16 -0400

    z

--- src/config/database/postgres/orm.config.ts
+++ src/config/database/postgres/orm.config.ts
@@ -11,7 +11,7 @@ export const databaseConfig: DataSourceOptions = {
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || '',
   entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/migrations/*.js'],
+  migrations: ['dist/migrations/*.js', 'src/migrations/*.ts'],
   synchronize: false,
 };
 
