a089255b8e26cc2d496afae0155f3b11adb66a54
Isaac Henderson <theisaachendo@gmail.com>
2025-05-28 18:30:52 -0400

    g

---
+++ src/migrations/1716923893000-AddMessageToNotificationsTypeEnum.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddMessageToNotificationsTypeEnum1716923893000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TYPE notifications_type_enum ADD VALUE IF NOT EXISTS 'MESSAGE';
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Note: PostgreSQL doesn't support removing values from enums
+    // We would need to create a new enum and replace the old one
+    // This is a complex operation and might require data migration
+    // For now, we'll leave this empty
+  }
+} 
