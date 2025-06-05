a8bb63635c075f3932e2c8a451387d83849d797b
Isaac Henderson <theisaachendo@gmail.com>
2025-06-05 11:30:04 -0400

    sfd

--- src/modules/posts/modules/reactions/reactions.module.ts
+++ src/modules/posts/modules/reactions/reactions.module.ts
@@ -5,7 +5,7 @@ import { UserModule } from '@app/modules/user/user.module';
 import { ReactionService } from './services/reactions.service';
 import { ReactionsController } from './controllers/reaction.controller';
 import { ReactionsMapper } from './dto/reaction.mapper';
-import { ArtistPostUserModule } from '../artist-post-user/artist-post-user.module';
+import { ArtistPostUserModule } from '../../artist-post-user/artist-post-user.module';
 import { NotificationModule } from '@app/modules/user/modules/notifications/notification.module';
 import { SharedModule } from '@app/shared/shared.module';
 import { ArtistPost } from '../artist-post/entities/artist-post.entity';
